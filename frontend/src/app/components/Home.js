724b9b2f768100b13001dd061bb58ee51b4e1f03
nnahmias <nnahmias@beable.fr>
2018-11-08 10:34:02 +0100

    Slider

--- frontend/src/app/components/Home.js
+++ frontend/src/app/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as IconSolid from '@fortawesome/free-solid-svg-icons';
 
 export class Home extends React.Component {
 
@@ -11,41 +13,32 @@ export class Home extends React.Component {
               title: 'Vulputate Mollis Ultricies Fermentum Parturient',
               description:
                   'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
-              button: 'Read More',
               image: 'https://i.imgur.com/ZXBtVw7.jpg',
-              user: 'Luan Gjokaj',
-              userProfile: 'https://i.imgur.com/JSW6mEk.png'
           },
           {
               title: 'Tortor Dapibus Commodo Aenean Quam',
               description:
                   'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
-              button: 'Discover',
               image: 'https://i.imgur.com/DCdBXcq.jpg',
-              user: 'Erich Behrens',
-              userProfile: 'https://i.imgur.com/0Clfnu7.png'
           },
           {
               title: 'Phasellus volutpat metus',
               description:
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
-              button: 'Buy now',
               image: 'https://i.imgur.com/DvmN8Hx.jpg',
-              user: 'Bruno Vizovskyy',
-              userProfile: 'https://i.imgur.com/4KeKvtH.png'
           }
         ];
 
         return (
             <div className={'home'}>
-                <Slider className="slider-wrapper">
+                <Slider
+                    className="slider-container"
+                    previousButton={<FontAwesomeIcon icon={IconSolid.faChevronLeft} />}
+                    nextButton={<FontAwesomeIcon icon={IconSolid.faChevronRight} />}
+                >
                     {
                         content.map((item, index) => (
-                            <div
-                                key={'slider_' + index}
-                                className="slider-content"
-                                style={{ background: `url('${item.image}') no-repeat center center` }}
-                            >
+                            <div key={'slider_' + index} className="slider-content" style={{ background: `url('${item.image}') no-repeat center center` }}>
                                 <div className="inner">
                                     <h1>{item.title}</h1>
                                     <p>{item.description}</p>
