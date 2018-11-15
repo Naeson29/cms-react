12a1cb94271d9169562752dd44bef1c33404ca6c
nnahmias <nnahmias@beable.fr>
2018-11-15 19:44:24 +0100

    Slider buttons

--- frontend/src/components/app/component/footer.js
+++ frontend/src/components/app/component/footer.js
@@ -1,19 +1,43 @@
 import React, {Component} from 'react';
 import { SocialIcon } from 'react-social-icons';
+import {DEVICE_TYPE} from "../../../utils/consts";
 
 
 class Footer extends Component {
 
     constructor(props) {
         super(props);
+
+
+        this.state = {
+            device : window.innerWidth > 768 ? DEVICE_TYPE.TYPE_DESKTOP : DEVICE_TYPE.TYPE_PHONE
+        };
+
+        this._onWindowResize = this._onWindowResize.bind(this);
     }
 
+    componentDidMount() {
+        window.addEventListener('resize', this._onWindowResize);
+    }
+
+    componentWillUnMount() {
+        window.removeEventListener('resize', this._onWindowResize);
+    }
+
+    _onWindowResize(e) {
+        let device = e.target.innerWidth > 768 ? DEVICE_TYPE.TYPE_DESKTOP : DEVICE_TYPE.TYPE_PHONE;
+
+        if(device !== this.state.device){
+            this.setState({device: device});
+        }
+    }
 
     render() {
+        const {device} = this.state;
 
         const style={
-            height: 60,
-            width: 60
+            height: (device === DEVICE_TYPE.TYPE_DESKTOP ? 65 : 50),
+            width: (device === DEVICE_TYPE.TYPE_DESKTOP ? 65 : 50)
         };
 
         return (
