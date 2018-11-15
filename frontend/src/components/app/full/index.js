406b8aa552a8a98a997e2d39615f51d54bf375ea
nnahmias <nnahmias@beable.fr>
2018-11-15 15:52:07 +0100

    Footer 1.0

--- frontend/src/components/app/full/index.js
+++ frontend/src/components/app/full/index.js
@@ -2,6 +2,7 @@ import React, {Component}   from 'react';
 import {Switch, Route}      from 'react-router-dom';
 
 import Header from '../component/header';
+import Footer from '../component/footer';
 import Home   from '../../../containers/home';
 
 class Full extends Component {
@@ -17,6 +18,9 @@ class Full extends Component {
                     <Route exact path="/" name="home" component={Home}/>
                     <Route exact path="/home" name="home" component={Home}/>
                 </Switch>
+                <div className="footer-app">
+                    <Footer/>
+                </div>
             </div>
         );
     }
